a2de31cea82f2a013f06efb75d99a7ec9b0fa3bd
Ashish <theashish752@gmail.com>
2024-01-19 11:04:37 +0530

    Created the public and private navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Users/Login";
 import Registration from "./components/Users/Resister";
 import Dashboard from "./components/Users/Dashboard";
+import PublicNavbar from "./components/navbar/Public.navbar";
+import PrivateNavbar from "./components/navbar/Private.navbar";
+
 
 export default function App() {
-  return <BrowserRouter>
+  return <>
+  <BrowserRouter>
+  <PrivateNavbar />
   <Routes>
+     
      <Route path="/login" element={<Login/>}/>
      <Route path="/register" element={<Registration/>}/>
      <Route path="/dashboard" element={<Dashboard/>}/>
   </Routes>
   </BrowserRouter>;
+  </>
 }
